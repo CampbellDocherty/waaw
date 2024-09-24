b2dd15a44a6bb6457635da3256193d60c8b00114
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-09-24 17:24:14 +0100

    Remove log

--- src/functions/requestDeviceMotionPermission.ts
+++ src/functions/requestDeviceMotionPermission.ts
@@ -31,7 +31,6 @@ export const requestDeviceMotionPermission = async (star: Star) => {
   }
   const isProbablyWeb = requestPermission === undefined;
 
-  console.log(isProbablyWeb, requestPermission);
   if (isProbablyWeb) {
     return isProbablyWeb;
   }
