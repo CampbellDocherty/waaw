568b97a777885fa79234a990375286b3c29d64e0
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-10-02 19:02:41 +0100

    Game over if block hit

---
+++ src/GameOver.tsx
@@ -0,0 +1,7 @@
+export const GameOver = () => (
+  <div className="game-over-screen" style={{ display: 'none' }}>
+    <h4>You scored</h4>
+    <h3 className="final-score">0</h3>
+    <button className="play-again-button">Play again</button>
+  </div>
+);
