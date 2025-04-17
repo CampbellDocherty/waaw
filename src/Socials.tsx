31bea90fa43592d0f431a1c49ac21e299805bba1
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-04-17 15:38:40 +0100

    Update

--- src/Socials.tsx
+++ src/Socials.tsx
@@ -4,24 +4,6 @@ export const Socials = () => (
     <ul>
       <li>
         <a
-          href="https://soundcloud.com/waawdj"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Soundcloud
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.mixcloud.com/waawtwins/stream/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mixcloud
-        </a>
-      </li>
-      <li>
-        <a
           href="https://www.instagram.com/waawdj/"
           target="_blank"
           rel="noreferrer"
