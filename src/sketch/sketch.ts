a8ec36d6ce66546521d9811cdaf6459afcfb2b81
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-04-17 15:37:02 +0100

    Adjust socuials

--- src/sketch/sketch.ts
+++ src/sketch/sketch.ts
@@ -1,4 +1,4 @@
-import * as p5Type from 'p5';
+import p5Type from 'p5';
 import monoRegular from '../fonts/Mono-Regular.ttf';
 import { EvilStar } from '../functions/EvilStar';
 import { TrackPowerUp } from '../functions/PowerUp';
